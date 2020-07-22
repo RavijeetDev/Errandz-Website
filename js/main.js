f76ac805ef0131520d0e5075c4fd4dd38db0094d
RavijeetDev <rahluwalia01@mylangara.bc.ca>
2020-07-21 17:53:22 -0700

    Updated UI and added clicklistener on faq

--- js/main.js
+++ js/main.js
@@ -1,18 +1,54 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+$(document).ready(function () {
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
+    $("#nav-features").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".feature-section").offset().top
+        }, 1000);
+    });
 
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
+    $("#nav-about").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".about-section").offset().top
+        }, 1000);
+    });
+
+    $("#nav-team").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".team-section").offset().top
+        }, 1000);
+    });
+
+    $("#nav-faq").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".faqs-section").offset().top
+        }, 1000);
+    });
+
+    $("#nav-contact").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".contact-section").offset().top
+        }, 1000);
+    });
+
+    $(".faqs-container .faq-container").click(function () {
+
+        let index = $(this).index() + 1
+        let elementParent = `.faq${index}`;
+
+        if(!$(`${elementParent} p`).is(':visible')) {
+            $(this).find("svg").css({
+                "transform": "rotate(0deg)"
+            });
+            $(`${elementParent} p`).show();
+        } else {
+            $(this).find("svg").css({
+                "transform": "rotate(180deg)"
+            });
+            $(`${elementParent} p`).hide();
+        }
+    })
+
+
+
+
+})
